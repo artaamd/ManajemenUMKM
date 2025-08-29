afe7ea43b1726a1fa74714a9b29d3d34d4419fc4
Arta Amadeo <arta@example.com>
2025-08-29 16:39:52 +0800

    tambah ikon logo di landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Manajemen Konten Digital UMKM Kota Gorontalo - Platform untuk mengelola konten dan analitik bisnis Anda.">
     <title>Manajemen Konten Digital UMKM Kota Gorontalo</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logokota.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
