e6a8078d9ee9ddb7aa4e95fa9473a8cb4c3847c7
Arta Amadeo <arta@example.com>
2025-09-14 22:15:23 +0800

    commit origin main

--- app/Http/Controllers/AnalitikController.php
+++ app/Http/Controllers/AnalitikController.php
@@ -108,6 +108,6 @@ class AnalitikController extends Controller
         if ($engagementScore >= 100) {
             return 'C'; // Cukup
         }
-        return 'D'; // Perlu Peningkatan
+        return 'D'; // Perlu Peningkata
     }
 }
